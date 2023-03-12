7e9a1821b5cf84ee09fa1a635d23d97e4aea322d
etho <>
2023-03-12 16:09:18 -0400

    success in first notification

--- SwiftStoryBoardPlayGround/AppDelegate.swift
+++ SwiftStoryBoardPlayGround/AppDelegate.swift
@@ -6,15 +6,41 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseMessaging
+import UserNotifications
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate , MessagingDelegate , UNUserNotificationCenterDelegate {
 
+   
 
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+
+        
+        Messaging.messaging().delegate = self
+        UNUserNotificationCenter.current().delegate = self
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.sound,.badge]){ success, _ in
+            guard success else{
+                return
+            }
+            print("Sucees APNS app registered")
+            
+        }
+        application.registerForRemoteNotifications()
         return true
+    }
+    
+    func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+        messaging.token{token , _ in
+            
+            guard let token = token else { return }
+            print("token \(token)")
+
+        }
     }
 
     // MARK: UISceneSession Lifecycle
